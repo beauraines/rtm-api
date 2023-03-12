fcfdf0624059d93e7e873ef18dc70bf16e727c44
Beau Raines <beau.raines@gmail.com>
2023-03-11 21:18:46 -0800

    feat: Adds debug to get/fetch functions

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
-const sign = require('../utils/sign');
-
+const sign = require('../utils/sign')
+const debug = require('debug')('rtm-api-fetch');
 
 // API Configuration Properties
 const config = require('../../config');
@@ -52,13 +52,15 @@ function formQuery(params) {
  */
 async function callAPI(url) {
   try {
-    const response = await fetch(url);
-    if (await response.ok) {
-      return await response.json();
-    } else {
-      // TODO improve this error message
-      console.error('There was an error');
-    }
+      const response = await fetch(url);
+      if (await response.ok) {
+          const responseJson = await response.json();
+	  debug(JSON.stringify(responseJson))
+	  return responseJson 
+      } else {
+          // TODO improve this error message
+          console.error('There was an error');
+      }
   } catch (error) {
     console.error(error);
   }
